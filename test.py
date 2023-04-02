a5b35b5fe779e59c64171957fce6acb03c468d3f
Remi Rampin <remi@rampin.org>
2023-04-02 18:07:21 -0400

    Implement terminal

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
+import sys
 from tkrminal import run_in_terminal
 
 
 if __name__ == '__main__':
-    run_in_terminal([], title="Taguette")
+    def open_browser():
+        pass
+
+    def quit():
+        sys.exit(0)
+
+    run_in_terminal(
+        ['sh', '-c', 'i=0; while [ $i -lt 5 ]; do i=$((i + 1)); sleep 1; echo "line $i"; done'],
+        title="Taguette",
+        actions=[
+            ("Open browser", open_browser),
+            ("Quit", quit),
+        ],
+    )
